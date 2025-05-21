259de6387ff378e5421ca2a8d9d7117d77b17aa3
Ars <sosyanarsen@gmail.com>
2025-05-21 06:09:17 +0500

    Second commit

---
+++ src/main/java/com/example/spring_product_api/SpringProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_product_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_product_api/SpringProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
